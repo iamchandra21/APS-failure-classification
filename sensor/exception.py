b8d292cda24224188f310c1dee8bc51f85a8d757
Tiyyagura Chandra Reddy <tchandrareddy21@gmail.com>
2025-08-11 17:19:11 +0530

    Fix: sys is used as type for error_detail 1. Removed sys type for error detail and used sys directly

--- sensor/exception.py
+++ sensor/exception.py
@@ -1,28 +1,24 @@
 import sys
 
 
-def error_message_detail(error, error_detail: sys):
-    _, _, exc_tb = error_detail.exc_info()
+def error_message_detail(error):
+    _, _, exc_tb = sys.exc_info()
 
     file_name = exc_tb.tb_frame.f_code.co_filename
 
-    error_message = "Error occurred python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
-    )
+    error_message = f"Error occurred python script name [{file_name}] line number [{exc_tb.tb_lineno}] error message [{str(error)}]"
 
     return error_message
 
 
 class SensorException(Exception):
-    def __init__(self, error_message, error_detail:sys):
+    def __init__(self, error_message):
         """
         :param error_message: error message in string format
         """
         super().__init__(error_message)
 
-        self.error_message = error_message_detail(
-            error_message, error_detail=error_detail
-        )
+        self.error_message = error_message_detail(error_message)
 
     def __str__(self):
         return self.error_message
